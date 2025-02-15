4b094dadd2b062161839fdb9168b32e8f12a4c6d
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-02-15 12:00:27 +0530

    add triangle program

---
+++ triangle .cpp
@@ -0,0 +1,25 @@
+/* Right angle triangle pattern
+* 
+* * 
+* * *
+* * * *
+* * * * *
+* * * * * *
+
+
+*/
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int row;
+    cout<<"Enter rows:";
+    cin>>row;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
