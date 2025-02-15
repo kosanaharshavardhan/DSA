be665c74049bdf511333819fcb00d584906d6812
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-02-15 12:09:26 +0530

    add pattern4

---
+++ pattern4.cpp
@@ -0,0 +1,23 @@
+/*
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int row;
+    cout<<"Enter rows:";
+    cin>>row;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<=i;j++){
+            cout<<i6+1<<" ";
+        }
+        cout<<endl;
+    }
+}
